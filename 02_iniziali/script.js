39429fc5faf87af67ed36cdbf837995dfc8b8008
BelliaNino <bellianino1996@gmail.com>
2026-02-10 15:40:52 +0100

    add ex 02_iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,8 +5,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function getInitial(arr){
+let result = [];
+
+for (let i = 0; i < arr.length; i++){
+    const char = arr[i];
+    result.push(char.charAt(0))
+}
+
+return result
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
+const initial = getInitial(names)
+console.log(initial);
 
 
 
